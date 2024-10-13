29b1ac0c187868a30c3d48216cc684dd04938174
Nikola Medarevic <nikola@petmediagroup.com>
2024-10-13 23:12:54 +0200

    Fix TestShouldMoveHeadBottomRight

--- snake/snake_test.go
+++ snake/snake_test.go
@@ -44,16 +44,24 @@ func TestShouldNotMoveHeadBackwards(t *testing.T) {
 }
 
 func TestShouldMoveHeadBottomRight(t *testing.T) {
-	snake := MakeSnake(3, 5, 3)
+	var snakeLength int32 = 0
 
-	table := make([][]uint8, 10)
-	for i := range table {
-		table[i] = make([]uint8, 10)
+	snakeBody := make([]SnakePoint, snakeLength)
+	snakeBody = append(snakeBody, SnakePoint{X: int32(0), Y: int32(0)})
+	snakeBody = append(snakeBody, SnakePoint{X: int32(0), Y: int32(1)})
+	snakeBody = append(snakeBody, SnakePoint{X: int32(0), Y: int32(2)})
+
+	snake := Snake{
+		Body: &snakeBody,
+		Id:   randSeq(10),
 	}
 
-	snake.Print()
 	snake.Move(constants.Right)
-	snake.Print()
+	if (*snake.Body)[0].X != 1 && (*snake.Body)[0].Y != 0 ||
+		(*snake.Body)[1].X != 0 && (*snake.Body)[1].Y != 0 ||
+		(*snake.Body)[2].X != 0 && (*snake.Body)[2].Y == 1 {
+		t.Errorf("Head is not in correct place")
+	}
 }
 
 func TestShouldMoveHeadBottomLeft(t *testing.T) {
